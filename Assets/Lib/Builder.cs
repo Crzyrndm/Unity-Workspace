20cc4f9983dc82d587e9b3b77b237b7ad353b8d5
Crzyrndm <joshcrawfy@gmail.com>
2016-10-23 14:41:38 +1300

    asset update

---
+++ Assets/Lib/Builder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    [MenuItem("Build/AssetBundle")]
+    public static void BuildAssets()
+    {
+        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+    }
+}
